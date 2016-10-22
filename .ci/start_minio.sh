b0113741152fc2c5e8a4bdfe7d29d8377d0d3270
Daniel Rodriguez <df.rodriguez143@gmail.com>
2016-10-22 13:30:09 -0500

    Fix permission of minio log file

--- .ci/start_minio.sh
+++ .ci/start_minio.sh
@@ -9,4 +9,4 @@ export MINIO_ACCESS_KEY=Q3AM3UQ867SPQQA43P2F
 export MINIO_SECRET_KEY=zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG
 
 mkdir -p ~/s3/notebooks
-minio server ~/s3 > /minio.log 2>&1 &
+minio server ~/s3 > /tmp/minio.log 2>&1 &

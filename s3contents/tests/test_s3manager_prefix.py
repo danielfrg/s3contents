a05aeb617269b58ae6a05e5889a3a5c61ddf1b96
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-03-21 21:58:54 -0500

    Add prefix tests

---
+++ s3contents/tests/test_s3manager_prefix.py
@@ -0,0 +1,43 @@
+from s3contents.ipycompat import TestContentsManager
+
+from s3contents import S3ContentsManager
+
+
+class S3ContentsManagerTestCase(TestContentsManager):
+
+    def setUp(self):
+        """
+        This setup is a hardcoded to the use a little minio server that run on travis CI or play.minio.io:9000
+        """
+        self.contents_manager = S3ContentsManager(
+            access_key_id="Q3AM3UQ867SPQQA43P2F",
+            secret_access_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+            endpoint_url="http://localhost:9000",
+            bucket_name="notebooks",
+            prefix="this/is/the/prefix"
+            signature_version='s3v4'
+        )
+
+    def tearDown(self):
+        bucket = self.contents_manager.s3fs.bucket
+
+        objects_to_delete = []
+        for obj in bucket.objects.filter(Prefix=""):
+            objects_to_delete.append({"Key": obj.key})
+
+        bucket.delete_objects(
+            Delete={
+                "Objects": objects_to_delete
+            }
+        )
+
+    # Overwrites from TestContentsManager
+
+    def make_dir(self, api_path):
+        self.contents_manager.new(
+            model={"type": "directory"},
+            path=api_path,
+        )
+
+# This needs to be removed or else we'll run the main IPython tests as well.
+del TestContentsManager

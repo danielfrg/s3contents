e0ae2529ef694fdcf3e1f5cdb07230045209ccf7
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-09-11 11:40:27 -0500

    XD

--- .ci/start_minio.sh
+++ .ci/start_minio.sh
@@ -8,6 +8,6 @@ export MINIO_ACCESS_KEY=access-key
 export MINIO_SECRET_KEY=secret-key
 
 mkdir -p ~/s3/notebooks
-./minio server ~/s3 # > /tmp/minio.log 2>&1 &
+./minio server ~/s3 > /tmp/minio.log 2>&1 &
 
 ps auxww

ff6235f4455887209ae0a095e042c8c72dc6ea40
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-12-01 17:42:08 -0600

    py2 and py3 fix?

--- s3contents/compat.py
+++ s3contents/compat.py
@@ -1,7 +1,11 @@
+import six
 import s3fs
 
-try:
-    FileNotFoundError = s3fs.core.FileNotFoundError
-except:
-    class FileNotFoundError(IOError):
-        pass
+if six.PY3:
+    FileNotFoundError = FileNotFoundError
+else:
+    try:
+        FileNotFoundError = s3fs.core.FileNotFoundError
+    except:
+        class FileNotFoundError(IOError):
+            pass

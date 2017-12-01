829fd920b712a850d6b81e1c343eee7070c7b90d
Daniel Rodriguez <df.rodriguez143@gmail.com>
2017-12-01 16:34:29 -0600

    Move /.ci to /ci

---
+++ ci/start_minio.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# go get -u github.com/minio/minio
+wget https://dl.minio.io/server/minio/release/linux-amd64/minio
+chmod +x minio
+
+export MINIO_ACCESS_KEY=access-key
+export MINIO_SECRET_KEY=secret-key
+
+mkdir -p ~/s3/notebooks
+./minio server ~/s3 > /tmp/minio.log 2>&1 &
+
+ps auxww

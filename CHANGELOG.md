7469fdea33bee909499d4289368cb02fc75cdedf
Daniel Rodriguez <df.rodriguez143@gmail.com>
2020-01-23 10:32:36 -0600

    Update setup.py

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,19 @@
-# Changelog
+# s3contents Change Log
 
-## [Unreleased]
-### Changed
+## Unreleased
+
+## 0.4
+- Drop support for Python 2, only Python >=3.5 is supported now
+- Updated dependencies
+
+## 0.3
 - Better error message on AccessDenied errors
 
-## [0.1.12] - 2018-11-14
-### Added
+## 0.1.12
 - Parameter for S3/AWS Session token
 
-## [0.1.11] - 2018-10-07
-### Added
+## 0.1.11
 - Support IPython 7
+
+## 0.1.0
+- Initial release
--- setup.py
+++ setup.py
@@ -11,21 +11,22 @@ def read_file(filename):
     with  open(filepath) as f:
         return f.read()
 
-REQUIREMENTS = read_file("requirements.txt").splitlines()
-
 
 setup(
     name="s3contents",
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     description="A S3-backed ContentsManager implementation for Jupyter",
-    long_description=read_file('README.md'),
+    long_description=read_file("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/danielfrg/s3contents",
     maintainer="Daniel Rodriguez",
     maintainer_email="df.rodriguez143@gmail.com",
     license="Apache 2.0",
+    python_requires=">=3.0,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
+    install_requires=read_file("requirements.txt").splitlines(),
+    keywords=["jupyter", "s3", "contents-manager"],
     packages=find_packages(),
-    install_requires=REQUIREMENTS,
+    include_package_data=True,
     zip_safe=False,
+    cmdclass=versioneer.get_cmdclass()
 )

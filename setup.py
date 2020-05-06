822288d186eb2ea22eb9aa84774464dcafc01c29
Daniel Rodriguez <daniel@danielfrg.com>
2020-05-06 12:36:08 -0500

    version test

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ import sys
 from setuptools import find_packages, setup
 
 
+setup_dir = os.path.abspath(os.path.dirname(__file__))
+
+
 def read_file(filename):
     this_dir = os.path.abspath(os.path.dirname(__file__))
     filepath = os.path.join(this_dir, filename)
@@ -11,18 +14,35 @@ def read_file(filename):
         return file.read()
 
 
+def parse_git(root, **kwargs):
+    """
+    Parse function for setuptools_scm that ignores tags for non-C++
+    subprojects, e.g. apache-arrow-js-XXX tags.
+    """
+    from setuptools_scm.git import parse
+    kwargs['describe_command'] =\
+        'git describe --dirty --tags --long'
+    return parse(root, **kwargs)
+
+
 setup(
     name="s3contents",
     packages=find_packages() + ["s3contents.tests"],
     zip_safe=False,
     include_package_data=True,
-    # package_data={"word2vec": ["includes/**/*.c"]},
-    # data_files=data_files,
+    # package_data={"s3contents": ["includes/*"]},
+    # data_files=[],
     # cmdclass={},
     # entry_points = {},
-    test_suite="word2vec/tests",
+    use_scm_version={
+        'root': setup_dir,
+        'parse': parse_git,
+        'write_to': os.path.join('s3contents/_generated_version.py')
+    },
+    test_suite="s3contents/tests",
+    setup_requires=["setuptools_scm"],
     install_requires=read_file("requirements.package.txt").splitlines(),
-    tests_require=["pytest",],
+    tests_require=["pytest"],
     python_requires=">=3.5",
     description="S3 Contents Manager for Jupyter",
     long_description=read_file("README.md"),

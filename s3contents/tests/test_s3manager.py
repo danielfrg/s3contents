e47a46247868dd13b363556e8c745336326cf634
Will Eaton <weaton@redhat.com>
2020-08-17 17:56:09 -0400

    add tests for hooks

--- s3contents/tests/test_s3manager.py
+++ s3contents/tests/test_s3manager.py
@@ -1,8 +1,12 @@
+import os
+import time
+
 import pytest
 
 from s3contents import S3ContentsManager
 from s3contents.ipycompat import TestContentsManager
 from s3contents.s3manager import _validate_bucket
+from s3contents.tests.hooks import make_html_post_save, scrub_output_pre_save
 
 
 @pytest.mark.minio
@@ -34,6 +38,41 @@ class S3ContentsManagerTestCase(TestContentsManager):
         self.contents_manager.new(
             model={"type": "directory"}, path=api_path,
         )
+
+
+    def test_save_hooks(self):
+        """
+        Extends TestContentsManager.save
+        """
+
+        self.contents_manager.pre_save_hook = scrub_output_pre_save
+        self.contents_manager.post_save_hook = make_html_post_save 
+
+        cm = self.contents_manager
+        model = cm.new_untitled(type='notebook')
+        path = model['path']
+
+        full_model = cm.get(path)
+        nb = full_model['content']
+        nb['metadata']['counter'] = int(1e6 * time.time())
+        self.add_code_cell(nb)
+
+        cm.save(full_model, path)
+
+        # test pre_save_hook 
+        loaded_model = cm.get(path)
+        for cell in loaded_model['content']['cells']:
+            assert cell['outputs'] == []
+
+        # test post_save_hook
+        html_file = os.path.splitext(path)[0] + '.html'
+        html, _type = cm.fs.read(html_file, 'text')
+        
+        assert cm.fs.isfile(html_file)
+        assert '<!DOCTYPE html>' in html
+
+        self.contents_manager.pre_save_hook = None
+        self.contents_manager.post_save_hook = None
 
 
 # This needs to be removed or else we'll run the main IPython tests as well.

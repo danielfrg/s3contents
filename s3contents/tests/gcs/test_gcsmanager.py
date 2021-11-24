39e0cf43809298772d0e9c3be95e0a7761b0df4b
Daniel Rodriguez <daniel@danielfrg.com>
2021-11-23 20:53:04 -0600

    Fix GCS tests

---
+++ s3contents/tests/gcs/test_gcsmanager.py
@@ -0,0 +1,39 @@
+import pytest
+from notebook.services.contents.tests.test_manager import TestContentsManager
+
+try:
+    from s3contents.gcs import GCSContentsManager
+except ImportError:
+    GCSContentsManager = None
+
+
+@pytest.mark.gcs
+class GCSContentsManagerTestCase(TestContentsManager):
+    def setUp(self):
+        """
+        This setup is a hardcoded to run on my laptop and GCP account :)
+        """
+        self.contents_manager = GCSContentsManager(
+            project="continuum-compute",
+            token="~/.config/gcloud/application_default_credentials.json",
+            bucket="gcsfs-test",
+        )
+
+        self.tearDown()
+
+    def tearDown(self):
+        for item in self.contents_manager.fs.ls(""):
+            self.contents_manager.fs.rm(item)
+        self.contents_manager.fs.init()
+
+    # Overwrites from TestContentsManager
+
+    def make_dir(self, api_path):
+        self.contents_manager.new(
+            model={"type": "directory"},
+            path=api_path,
+        )
+
+
+# This needs to be removed or else we'll run the main IPython tests as well.
+del TestContentsManager

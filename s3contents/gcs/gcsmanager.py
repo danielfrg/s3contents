65b32e104742a75d643cf74dcf9811c98fa43e38
yucensun <43123355+yucensun@users.noreply.github.com>
2023-07-03 01:04:16 -0400

    fix gcs list (#170)

--- s3contents/gcs/gcsmanager.py
+++ s3contents/gcs/gcsmanager.py
@@ -1,6 +1,8 @@
 from s3contents.gcs.gcs_fs import GCSFS
 from s3contents.genericmanager import GenericContentsManager
 from s3contents.ipycompat import Unicode
+from fsspec.asyn import sync
+import os
 
 
 class GCSContentsManager(GenericContentsManager):
@@ -25,7 +27,6 @@ class GCSContentsManager(GenericContentsManager):
 
     def __init__(self, *args, **kwargs):
         super(GCSContentsManager, self).__init__(*args, **kwargs)
-
         self._fs = GCSFS(
             log=self.log,
             project=self.project,
@@ -34,3 +35,44 @@ class GCSContentsManager(GenericContentsManager):
             prefix=self.prefix,
             separator=self.separator,
         )
+
+    def _convert_file_records(self, gcs_paths):
+        """Convert gcs paths to models of the filetypes."""
+        models = []
+        gcs_paths = self.fs.remove_prefix(gcs_paths)
+        self.log.debug("gcs paths", gcs_paths)
+        for path in gcs_paths:
+            if os.path.basename(path) == self.fs.dir_keep_file:
+                continue
+            _type = self.guess_type(path, allow_directory=True)
+            if _type == "notebook":
+                models.append(self._notebook_model_from_path(path, False))
+            elif _type == "file":
+                models.append(self._file_model_from_path(path, False, None))
+            elif _type == "directory":
+                models.append(self._directory_model_from_path(path, False))
+            else:
+                self.do_error(
+                    "Unknown file type '%s' for file '%s'" % (_type, path), 500
+                )
+        return models
+
+    def _list_contents(self, model, prefixed_path):
+        # Specific to GCS filesystem
+        files_gcs_detail = sync(self.fs.fs.loop, self.fs.fs._ls, prefixed_path)
+        filtered_files_gcs_detail = list(
+            filter(
+                lambda detail: os.path.basename(detail) != "",
+                files_gcs_detail,
+            )
+        )
+        self.log.debug(f"listed files: {filtered_files_gcs_detail}\n")
+        for files_gcs_detail in filtered_files_gcs_detail:
+            self.log.debug(
+                f"files_tcs_detail={files_gcs_detail}\n"
+                f"lstat={self.fs.lstat(files_gcs_detail)}\n"
+            )
+        model["content"] = self._convert_file_records(
+            filtered_files_gcs_detail
+        )
+        return model

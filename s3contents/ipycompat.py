4f7683a520708933293dacf80707abf21fd7e338
antoine_galataud <antoine@foobot.io>
2023-07-25 17:22:15 +0200

    remove obsolete imports

--- s3contents/ipycompat.py
+++ s3contents/ipycompat.py
@@ -3,10 +3,6 @@ Utilities for managing compat between notebook versions.
 
 Taken from: https://github.com/quantopian/pgcontents/blob/master/pgcontents/utils/ipycompat.py
 """
-import sys
-import platform
-
-from ipython_genutils.py3compat import string_types
 from nbformat import from_dict, reads, writes
 from nbformat.v4.nbbase import (
     new_code_cell,
